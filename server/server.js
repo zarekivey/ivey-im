f028569cb47e075143d47c70c115014a5c13329e
Malachi Ivey <zarekbusiness@gmail.com>
2019-01-31 09:47:39 -0500

    greet new users, alert others

--- server/server.js
+++ server/server.js
@@ -14,14 +14,30 @@ app.use(express.static(publicPath));
 // events with socket io
 io.on('connection', (socket) => {
     console.log('New user connected.');
+    
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the messenger app'
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined'
+    });
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
         });
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // })
     });
 
     socket.on('disconnect', () => {
