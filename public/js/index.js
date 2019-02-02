40b32a3e4d6fc3a9b479c772b9014721d5b47555
Malachi Ivey <zarekbusiness@gmail.com>
2019-02-02 09:21:22 -0500

    autoscrolling adding

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,19 @@
 const socket = io();
 
+function scrollToBottom () {
+    const messages = jQuery('#messages');
+    const newMessage = messages.children('li:last-child');
+    const clientHeight = messages.prop('clientHeight');
+    const scrollTop = messages.prop('scrollTop');
+    const scrollHeight = messages.prop('scrollHeight');
+    const newMessageHeight = newMessage.innerHeight();
+    const lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    };
+};
+
 // client side js
 socket.on('connect', function () {
     console.log('Connected to server');
@@ -19,6 +33,7 @@ socket.on('newMessage', function (message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 // location message
@@ -34,6 +49,7 @@ socket.on('newLocationMessage', function (message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 const messageTextbox = jQuery('[name=message')
