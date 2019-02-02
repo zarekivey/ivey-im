695efb983814e1a41a12635956888448761a49b3
Malachi Ivey <zarekbusiness@gmail.com>
2019-02-02 14:40:37 -0500

    messages only sned to people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -80,7 +80,6 @@ jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
 
     socket.emit('createMessage', {
-        from: 'User',
         text: messageTextbox.val()
     }, function () {
         messageTextbox.val('')
--- server/server.js
+++ server/server.js
@@ -40,14 +40,22 @@ io.on('connection', (socket) => {
     });
 
     socket.on('createMessage', (message, callback) => {
-        console.log('createMessage', message);
+        let user = users.getUser(socket.id);
 
-        io.emit('newMessage', generateMessage(message.from, message.text));
+        // if user exist and the string message is valid
+        if (user && isRealString(message.text)) {
+            io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+        }
+
         callback();
     });
 
     socket.on('createLocationMessage', (coords) => {
-        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+        let user = users.getUser(socket.id) 
+
+        if (user) {
+            io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+        }
     });
 
     socket.on('disconnect', () => {
