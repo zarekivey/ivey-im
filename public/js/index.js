fffb47f88a4e4679c2dd641f9da2e1028d003900
Malachi Ivey <zarekbusiness@gmail.com>
2019-02-02 00:33:58 -0500

    Added location sharing messsage vio geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -2,29 +2,60 @@ const socket = io();
 
 // client side js
 socket.on('connect', function () {
-    console.log('connected to server');
+    console.log('Connected to server');
 });
 
 socket.on('disconnect', function () {
-    console.log('disconnected from the server');
+    console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function (message) {
+    socket.on('newMessage', function (message) {
     console.log('newMessage', message);
-    // adding messages to the ol
-    let li = jQuery('<li><li>');
-    li.text(`${message.from}: ${message.text}`)
+
+    // adding messaages to the ol
+    let li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messages').append(li);
+});
+
+// location message
+socket.on('newLocationMessage', function (message) {
+    let li = jQuery('<li></li>');
+    let a = jQuery('<a target="_blank">My current location</a>');
+
+    // a link to the location
+    li.text(`${message.from}: `);
+    a.attr('href', message.url);
+    li.append(a);
     jQuery('#messages').append(li);
 });
 
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
-    
+
     socket.emit('createMessage', {
         from: 'User',
-        // form the message form
         text: jQuery('[name=message]').val()
     }, function () {
 
     });
+});
+
+const locationButton = jQuery('#send-location');
+locationButton.on('click', function () {
+    // if they're on an older browser
+    if (!navigator.geolocation) {
+        return alert('Geolocation not supported by your browser');
+    }
+    // if they accept
+    navigator.geolocation.getCurrentPosition(function (position) {
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+        });
+    }, function () {
+    // if they deny permission
+        alert('Location access denied');
+    })
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,14 +1,27 @@
 const expect = require('expect');
 
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generate message', () => {
     it('should generate the correct message object', () => {
-        let from = 'Me';
-        let text = 'text';
-        let message = generateMessage(from , text);
+        const from = 'Me';
+        const text = 'text';
+        const message = generateMessage(from , text);
 
         expect(message.createdAt).toBeA('number');
         expect(message).toInclude({ from, text})
+    });
+});
+
+describe('generateLcationMessage', () => {
+    it('should generate correct location object', () => {
+        const from ='TestName';
+        const latitude = 1;
+        const longitude = 2;
+        const url = 'https://www.google.com/maps?q=1,2'
+        const message = generateLocationMessage(from, latitude, longitude)
+
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({ from, url})
     });
 });
